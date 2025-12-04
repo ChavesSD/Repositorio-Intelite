934787a85a8cb9d4f107aabdea1c8d3269228950
ChavesSD <chavessd@users.noreply.github.com>
2025-12-04 16:54:21 -0300

    Adiciona páginas de Links, Tutoriais e APKs com ícones dos produtos

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 function navigateTo(page) {
-    // Por enquanto, apenas mostra um alerta
-    // Quando as páginas forem criadas, redirecionará para elas
-    const pageNames = {
-        'links.html': 'Links',
-        'tutoriais.html': 'Tutoriais',
-        'apks.html': 'Apks'
-    };
-    
-    // Verifica se a página existe
-    fetch(page, { method: 'HEAD' })
-        .then(response => {
-            if (response.ok) {
-                window.location.href = page;
-            } else {
-                alert(`A página ${pageNames[page]} será criada em breve!`);
-            }
-        })
-        .catch(() => {
-            alert(`A página ${pageNames[page]} será criada em breve!`);
-        });
+    // Redireciona diretamente para a página
+    // Funciona tanto com arquivos locais (file://) quanto com servidor web (http://)
+    window.location.href = page;
 }
 
 // Adiciona animação de entrada aos botões
